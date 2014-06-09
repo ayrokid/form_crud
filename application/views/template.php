4d3e772252121cec3c6ce20223831a3054c748d8
yanun <ayrokid@gmail.com>
2014-06-09 11:32:36 +0700

    tambahkan menu artikel

--- application/views/template.php
+++ application/views/template.php
@@ -21,94 +21,16 @@
     <link rel="stylesheet" type="text/css" href="<?php echo path_js('easyui/themes/icon.css'); ?>">
     <script type="text/javascript" src="<?php echo path_js('easyui/jquery.easyui.min.js'); ?>"></script>
     <script src="<?php echo path_js('fun.jquery.js'); ?>" type="text/javascript"></script>
-
-    <script>
-    function confirm_page(page,msg,url){
-        $.messager.confirm('Confirm', msg, function(r){
-            if (r){
-                $.ajax({
-                    url: page,
-                    dataType: "html",
-                    timeout: 10000,
-                    success: function(data){
-                        var data = eval('(' + data + ')');
-                        if(data.back==='true'){
-                            $.messager.alert('Info','Successfully','info', function(){ window.location = url; });                            
-                        }else{
-                            $.messager.alert('Error',data.msg,'error');
-                        }
-                    },error: function(x, t, m) {
-                        if(t==="timeout") {
-                            $("#form-info").hide();
-                            $.messager.alert('Error','timeout','error');
-                        }else {
-                            $("#form-info").hide();
-                            $.messager.alert('Error',m,'error');
-                        }
-                    }   
-                });
-            }
-        });
-    }
-    function load_page(url){
-        $("#here").html("<center><p class='load'></p></center>");
-        $.ajax({
-            url: url,
-            dataType: "HTML",                       
-            success: function(data){
-                $("#here").html(data);
-            }         
-        });
-    }
-    function new_page(page, pjg){
-        var params  = 'width='+pjg;
-        params += ', height=650';
-        params += ', fullscreen=yes,scrollbars=yes';
-        window.open(page,'_blank', params);
-    }  
-    function myformatter(date){
-        var y = date.getFullYear();
-        var m = date.getMonth()+1;
-        var d = date.getDate();
-        return (d<10?('0'+d):d)+'/'+(m<10?('0'+m):m)+'/'+y;
-    }
-    function datetime(date){
-        var y = date.getFullYear();
-        var m = date.getMonth()+1;
-        var d = date.getDate();
-        var h = date.getHours();
-        var i = date.getMinutes();
-        var s = date.getSeconds();
-        return (d<10?('0'+d):d)+'/'+(m<10?('0'+m):m)+'/'+y+' '+h+':'+i+':'+s;
-    }
-    </script>
 </head>
 <body>
 
 <div id="container">
     
     <div id="left-menu">
-        <h1>SILVA<span>TEL</span></h1>
+        <h1>CMS <span>EDITOR</span></h1>
         <ul class="master">
-            <li><?php echo anchor("master/room", 'room'); ?></li>
-            <li><?php echo anchor("master/food", 'food'); ?></li>
-            <li><?php echo anchor('master/laundry', 'laundry'); ?></li>
-            <li><?php echo anchor("master/restaurant", 'restaurant'); ?></li>
-            <li><?php echo anchor("master/spa", 'SPA Type'); ?></li>
+            <li><?php echo anchor("artikel", 'artikel'); ?></li>
         </ul>
-        <ul class="trans">
-            <li><?php echo anchor('transaksi/check_in', 'check in'); ?></li>
-            <li><?php echo anchor('transaksi/receipt', 'receipt form'); ?></li>
-            <li><?php echo anchor('transaksi/guest_list', 'guest list'); ?></li>
-            <li><?php echo anchor('transaksi/restaurant', 'restaurant'); ?></li>
-            <li><?php echo anchor('transaksi/miscell', 'miscellaneous'); ?></li>
-            <li><?php echo anchor('transaksi/laundry', 'laundry'); ?></li>
-            <li><?php echo anchor('transaksi/spa', 'SPA'); ?></li>            
-            <li><?php echo anchor('transaksi/check_out', 'check out'); ?></li>
-        </ul>
-<!--        <ul class="report">
-            <li><?php echo anchor('laporan/sales_laundry', 'sales laundry'); ?></li>
-        </ul>-->
     </div>
     
     <div id="body">
