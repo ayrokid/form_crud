20c2027e47145fc430ff91f142cd30cf4d763af2
yanun <ayrokid@gmail.com>
2014-06-09 11:43:00 +0700

    update height: 430px;

--- application/views/artikel/list.php
+++ application/views/artikel/list.php
@@ -1,17 +1,12 @@
 <div id="tb">  
-    <span>Search :</span>  
-    <input id="nama" type="text" class="text-medium" placeholder="" onkeyup="cari()" />
+    <a href="<?php echo site_url("artikel/new"); ?>" class="easyui-linkbutton" iconCls="icon-add" >New</a>
 </div>
-<table id="grid" class="easyui-datagrid" url="<?php echo site_url("transaksi/load_guest"); ?>" style="height: 450px; margin: 0 auto" rownumbers="true" singleSelect="true" toolbar="#tb" pagination="true">  
+<table id="grid" class="easyui-datagrid" url="<?php echo site_url("artikel/load_artikel"); ?>" style="height: 430px; margin: 0 auto" rownumbers="true" singleSelect="true" toolbar="#tb" pagination="true">  
     <thead>  
-        <tr>    
-            <th field="room" width="60">Room</th>  
-            <th field="guest" width="270">Guest Name</th>
-            <th field="deposit" width="100" align="right">Deposit (IDR)</th>  
-            <th field="balance" width="100" align="right">Balance (IDR)</th>  
-            <th field="in" width="200" align="right">Check In</th>
-            <th field="out" width="200" align="right">Check Out</th>
-            <th field="action" align="center" width="50">Action</th>
+        <tr>     
+            <th field="ar_title" width="650">Title</th>
+            <th field="date" width="200">Expired Date</th>  
+            <th field="action" align="center" width="100">Action</th>
         </tr>                            
     </thead>                                                       
 </table> 
